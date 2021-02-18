9c0d0b95c6be2e2f433a04f62d31ec373843ccdf
Salvador Morales Aguayo <sma97@byu.edu>
2021-02-17 22:46:20 -0700

    Add project files.

---
+++ Models/Book.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment5.Models
+{
+    public class Book // all of the inputs are required
+    {
+        [Key]
+        public int BookId { get; set; } //this is the primary key
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Author { get; set; }
+        [Required]
+        public string Publisher { get; set; }
+        [DataType(DataType.Custom)]
+        [Display(Name = "Isbn Number")]
+        [Required(ErrorMessage = "ISBN Number Required!")]
+        [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{10})$", // this is the format that will be required.
+                   ErrorMessage = "Entered ISBN format is not valid.")]
+        public string Isbn { get; set; } //make sure it's validated and required
+        [Required]
+        public string Category { get; set; }
+        [Required]
+        public double Price { get; set; }
+    }
+}
---
+++ Models/EFCharityRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment5.Models
+{
+    public class EFCharityRepository : ICharityRepository
+    {
+        private CharityDbContext _context;
+
+        //constructor
+        public EFCharityRepository (CharityDbContext context)
+        {
+            _context = context;
+        }
+        public IQueryable<Book> Books => _context.Books;
+    }
+}
