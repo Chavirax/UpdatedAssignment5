4b18d80fa80445199dab546ef9cf444434a8d36a
Salvador Morales Aguayo <sma97@byu.edu>
2021-03-03 14:49:14 -0700

    New folder for navigation and filtering

---
+++ Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Assignment5.Models;
+
+namespace Assignment5.Components
+{
+    public class NavigationMenuViewComponent : ViewComponent
+    {
+        private ICharityRepository repository;
+        public NavigationMenuViewComponent (ICharityRepository r)
+        {
+            repository = r;
+        }
+        public IViewComponentResult Invoke() //drops a partial view into the view
+        {
+            ViewBag.SelectedType = RouteData?.Values["category"];
+
+            return View(repository.Books
+                .Select(x => x.Category)
+                .Distinct()
+                .OrderBy(x => x));
+        }
+    }
+}
